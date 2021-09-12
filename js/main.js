e9c3c97527668a6b93256da60e31894cff04f646
M074MED <mhmedmhmed1234951@gmail.com>
2021-09-12 05:28:59 +0200

    add the project

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function run() {
+    var input = document.getElementById("text-area-input").value,
+        output = document.getElementById("output").innerHTML = input;
+    
+    return output;
+}
